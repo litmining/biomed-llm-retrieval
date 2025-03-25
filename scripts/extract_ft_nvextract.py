8f004e75004a767d9c3466f9fe61c99088f40f31
Alejandro de la Vega <aleph4@gmail.com>
2025-03-25 15:53:06 -0500

    Simplify nv extract

--- scripts/extract_ft_nvextract.py
+++ scripts/extract_ft_nvextract.py
@@ -33,7 +33,7 @@ def _clean_output(output):
     return output
     
 
-def _tokenize_and_predict(prompt, model, tokenizer, max_length, max_new_tokens):
+def nuextract_generate(prompt, model, tokenizer, max_length, max_new_tokens):
     input_ids = tokenizer(prompt, return_tensors="pt", truncation=True, padding=True, max_length=max_length).to('cuda')
     return tokenizer.decode(model.generate(**input_ids, max_new_tokens=max_new_tokens)[0], skip_special_tokens=True)
 
@@ -41,7 +41,7 @@ def _predict_chunk(text, template, current, model, tokenizer, max_length, max_ne
     current = _clean_json_text(current)
 
     input_llm =  f"<|input|>\n### Template:\n{template}\n### Current:\n{current}\n### Text:\n{text}\n\n<|output|>" + "{"
-    output = _tokenize_and_predict(input_llm, model, tokenizer, max_length, max_new_tokens)
+    output = nuextract_generate(input_llm, model, tokenizer, max_length, max_new_tokens)
     torch.cuda.empty_cache()
 
     return _clean_output(output)
@@ -53,7 +53,6 @@ def split_document(document, window_size, overlap, tokenizer):
     chunks = []
     if len(tokens) > window_size:
         for i in range(0, len(tokens), window_size-overlap):
-            print(f"\t{i} to {i + len(tokens[i:i + window_size])}")
             chunk = tokenizer.convert_tokens_to_string(tokens[i:i + window_size])
             chunks.append(chunk)
 
@@ -95,24 +94,44 @@ def sliding_window_prediction(text, template, model, tokenizer, window_size=2000
 
 def full_text_prediction(text, template, model, tokenizer, max_length=10_000, max_new_tokens=4000):
     prompt = f"""<|input|>\n### Template:\n{template}\n### Text:\n{text}\n\n<|output|>"""
-    output =  _tokenize_and_predict(prompt, model, tokenizer, max_length, max_new_tokens)
+    output =  nuextract_generate(prompt, model, tokenizer, max_length, max_new_tokens)
     return _clean_output(output)
 
+def load_model(model_name):
+    torch.cuda.empty_cache()
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, torch_dtype=torch.bfloat16, trust_remote_code=True,
+        device_map="auto").eval()
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    return model, tokenizer
 
-    
-def predict_NuExtract(model, tokenizer, texts, template, mode, ids=None, outfile=None, **kwargs):
-    template = json.dumps(template, indent=4)
+
+def run_NuExtract(
+    texts: list,
+    schema: str,
+    model_name: str = "numind/NuExtract-v1.5",
+    mode = "sliding_window",
+    ids = None,
+    outfile = None,
+    **kwargs
+):
+    template = schema_to_template(schema.model_json_schema(), version="v1")
+    model, tokenizer = load_model(model_name)
     
     results = []
     with torch.no_grad():
         for i, text in tqdm(enumerate(texts)):
             try:
                 if mode == 'full_text':
-                    output = full_text_prediction(text, template, model, tokenizer, **kwargs)
-                elif mode == 'sliding_window':
-                    output = sliding_window_prediction(text, template, model, tokenizer, **kwargs)
+                    _func = full_text_prediction
+                else:
+                    _func = sliding_window_prediction
 
-                result = json.loads(output)
+                result = json.loads(
+                    _func(
+                        text, template, model, tokenizer, **kwargs
+                    )
+                )
 
                 if ids:
                     result['id'] = ids[i]
@@ -129,36 +148,13 @@ def predict_NuExtract(model, tokenizer, texts, template, mode, ids=None, outfile
     return results
 
 
-def run_NuExtract(
-    texts: list,
-    schema: str,
-    model_name: str = "numind/NuExtract-v1.5",
-    mode = "sliding_window",
-    ids = None,
-    outfile = None,
-    **kwargs
-):
-    torch.cuda.empty_cache()
-
-    template = schema_to_template(schema.model_json_schema(), version="v1")
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name, torch_dtype=torch.bfloat16, trust_remote_code=True,
-        device_map="auto").eval()
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-
-    return predict_NuExtract(
-        model, tokenizer, texts, template, mode, ids=ids, outfile=outfile, **kwargs
-        )
-
-
 def _run(extraction_model, docs, schema, prepend='', **extract_kwargs):
     short_model_name = extraction_model.split('/')[-1]
     
     outfile = OUTPUT_DIR / \
         f"full_{prepend}_{short_model_name}.jsonl"
 
-    # Extract
-    predictions = run_NuExtract(
+    run_NuExtract(
         docs['text'].to_list(),
         schema, model_name=extraction_model,
         ids=[d['pmcid'] for d in docs['metadata']],
